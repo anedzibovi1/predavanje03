72aa4626ce0fb48cad204b184ca4b87ae33e8f85
anedzibovi1 <anedzibovi1@etf.unsa.ba>
2019-10-09 15:03:08 +0200

    innitial commit

--- src/ba/unsa/etf/rpr/predavanje03/Main.java
+++ src/ba/unsa/etf/rpr/predavanje03/Main.java
@@ -3,6 +3,5 @@ package ba.unsa.etf.rpr.predavanje03;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+        System.out.println("hehe");    }
 }
